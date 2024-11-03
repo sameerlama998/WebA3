71d7a91395441c64d8ae2286f18643bad4507ab5
Sameer Tamang <lamasameer998@gmail.com>
2024-11-03 15:17:04 -0500

    a4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 // /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./views/**/*.html", // Include your HTML files
+    // "./views/**/*.html", // Include your HTML files
+    "./views/**/*.ejs", // Include your HTML files
     "./public/**/*.html",
     "./src/**/*.{js,jsx,ts,tsx}", // Adjust as needed for your JavaScript/TypeScript files
   ],
