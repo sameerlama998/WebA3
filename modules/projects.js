7cfc8e7466e00a7057fc8a44627284367447cdd5
Sameer Tamang <lamasameer998@gmail.com>
2024-11-15 15:13:04 -0500

    test

--- modules/projects.js
+++ modules/projects.js
@@ -2,55 +2,86 @@ require('dotenv').config();
 require('pg');
 const Sequelize = require('sequelize');
 
+
 const projectData = require("../data/projectData");
 const sectorData = require("../data/sectorData");
 
 let projects = [];
 
+// Initialize projects with sectors
 function initialize() {
   return new Promise((resolve, reject) => {
     projectData.forEach(projectElement => {
-      let projectWithSector = { ...projectElement, sector: sectorData.find(sectorElement => sectorElement.id == projectElement.sector_id).sector_name }
+      let projectWithSector = {
+        ...projectElement,
+        sector: sectorData.find(sectorElement => sectorElement.id == projectElement.sector_id).sector_name
+      };
       projects.push(projectWithSector);
     });
     resolve();
   });
 }
 
+// Get all projects
 function getAllProjects() {
   return new Promise((resolve, reject) => {
     resolve(projects);
   });
 }
 
+// Get project by ID
 function getProjectById(projectId) {
-
   return new Promise((resolve, reject) => {
     let foundProject = projects.find(p => p.id == projectId);
-
     if (foundProject) {
-      resolve(foundProject)
+      resolve(foundProject);
     } else {
       reject("Unable to find requested project");
     }
-
   });
-
 }
 
+// Get projects by sector
 function getProjectsBySector(sector) {
-
   return new Promise((resolve, reject) => {
     let foundProjects = projects.filter(p => p.sector.toUpperCase().includes(sector.toUpperCase()));
-
-    if (foundProjects) {
-      resolve(foundProjects)
+    if (foundProjects.length > 0) {
+      resolve(foundProjects);
     } else {
       reject("Unable to find requested projects");
     }
   });
-
 }
 
+// Get all sectors
+function getAllSectors() {
+  return new Promise((resolve, reject) => {
+    resolve(sectorData);
+  });
+}
 
-module.exports = { initialize, getAllProjects, getProjectById, getProjectsBySector }
+// Add a new project
+function addProject(projectData) {
+  return new Promise((resolve, reject) => {
+    try {
+      // Assuming sector_id is included in projectData to link to a sector
+      let sector = sectorData.find(s => s.id == projectData.sector_id);
+      if (!sector) {
+        return reject("Invalid sector ID");
+      }
+
+      // Create a new project object
+      let newProject = {
+        id: projects.length + 1,  // Example ID generation
+        ...projectData,
+        sector: sector.sector_name
+      };
+      projects.push(newProject);
+      resolve();
+    } catch (err) {
+      reject("Error adding project: " + err);
+    }
+  });
+}
+
+module.exports = { initialize, getAllProjects, getProjectById, getProjectsBySector, getAllSectors, addProject };
--- server.js
+++ server.js
@@ -5,50 +5,74 @@ const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
-
 app.set('view engine', 'ejs');
 
+// Middleware for parsing URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+
+// Home route
 app.get('/', (req, res) => {
   res.render("home");
 });
 
+// About route
 app.get('/about', (req, res) => {
   res.render("about");
 });
 
-app.get("/solutions/projects", async (req,res)=>{
-
-  try{
-    if(req.query.sector){
+// GET /solutions/projects - Display all projects or filter by sector
+app.get("/solutions/projects", async (req, res) => {
+  try {
+    if (req.query.sector) {
       let projects = await projectData.getProjectsBySector(req.query.sector);
-      (projects.length > 0) ? res.render("projects", {projects: projects}) : res.status(404).render("404", {message: `No projects found for sector: ${req.query.sector}`});
-  
-    }else{
+      (projects.length > 0) ? res.render("projects", { projects: projects }) : res.status(404).render("404", { message: `No projects found for sector: ${req.query.sector}` });
+    } else {
       let projects = await projectData.getAllProjects();
-      res.render("projects", {projects: projects});
+      res.render("projects", { projects: projects });
     }
-  }catch(err){
-    res.status(404).render("404", {message: err});
+  } catch (err) {
+    res.status(404).render("404", { message: err });
   }
-
 });
 
-app.get("/solutions/projects/:id", async (req,res)=>{
-  try{
+// GET /solutions/projects/:id - Display a single project by ID
+app.get("/solutions/projects/:id", async (req, res) => {
+  try {
     let project = await projectData.getProjectById(req.params.id);
-    res.render("project", {project: project})
-  }catch(err){
-    res.status(404).render("404", {message: err});
+    res.render("project", { project: project });
+  } catch (err) {
+    res.status(404).render("404", { message: err });
   }
 });
 
-app.use((req, res, next) => {
-  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+// Route to display the Add Project form
+app.get("/solutions/addProject", async (req, res) => {
+  try {
+    let sectors = await projectData.getAllSectors();
+    res.render("addProject", { sectors: sectors });
+  } catch (err) {
+    res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
+  }
 });
 
-projectData.initialize().then(()=>{
-  app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
+// Route to process the Add Project form
+app.post("/solutions/addProject", async (req, res) => {
+  try {
+    await projectData.addProject(req.body);
+    res.redirect("/solutions/projects");
+  } catch (err) {
+    res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
+  }
+});
+
+// 404 error route for unmatched routes
+app.use((req, res) => {
+  res.status(404).render("404", { message: "I'm sorry, we're unable to find what you're looking for" });
+});
+
+// Initialize project data and start server
+projectData.initialize().then(() => {
+  app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`); });
 });
